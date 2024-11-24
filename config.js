f37582ae2e02fcf7d34f8307cdc6136c847918f2
anthony2303 <161376491+anthony2303@users.noreply.github.com>
2024-11-24 12:26:46 -0600

    Update config.js

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 global.owner = [
-["5218336105471", 'Tilin', true], //el tilin
-["5218336105471", 'gay', true], //es gay
-["5218336105471"]]
+["5215662050230", 'Mono', true], //el monovan
+["5217202953045", 'gay', true], //es gay
+["5215625060788"]]
 
 global.mods = ["5218336105471"]
 global.prems = ["5218336105471"]
@@ -112,9 +112,9 @@ global.moment = moment
 // [ES] > Agregate a ti, colaboradores o ayudates, aparecerá en el comando de lista de contactos.
 // [EN] > Adding yourself, collaborators or helpers will appear in the contact list command.
 global.official = [ // Agregate si eres Owner
-["5218336105471", 'tilin', 1], 
-["5218336105471", 'tilinn', 1],  
-["5218336105471", 'Tilins', 1]]
+["5217202953045", 'mono', 1], 
+["5217202953045", 'sexoooo', 1],  
+["5217202953045", 'gaaaay', 1]]
 
 global.mail = '' // Add email
 global.desc = '' // Add short description (20 caractres max)
@@ -122,8 +122,8 @@ global.desc2 = '' // Add long description (90 caractres max) (Este parámetro se
 global.country = '' // Add country, example: 🇪🇨
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
-global.packname = "𝙂𝙊𝙆𝙐-𝘽𝙤𝙩"
-global.author = "𝙂𝙊𝙆𝙐-𝘽𝙤𝙩"
+global.packname = "𝙈𝙤𝙣𝙤𝙎𝙚𝙭𝙤-𝘽𝙤𝙩"
+global.author = "𝙈𝙤𝙣𝙤𝙎𝙚𝙭𝙤-𝘽𝙤𝙩"
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 // [ES] > INFORMACIÓN DE VERSIONES DEL BOT, POR FAVOR 
@@ -131,7 +131,7 @@ global.author = "𝙂𝙊𝙆𝙐-𝘽𝙤𝙩"
 // [EN] > BOT VERSION INFORMATION, PLEASE KEEP THIS UNCHANGED, WE STRIVE DAILY TO PROVIDE YOU WITH A BOT FOR THE COMMUNITY, BE GRATEFUL
 global.vs = "1.7.0"
 global.vsJB = "2.5 (Beta)"
-global.gt = "𝙂𝙊𝙆𝙐-𝘽𝙤𝙩"
+global.gt = "𝙈𝙤𝙣𝙤𝙎𝙚𝙭𝙤-𝘽𝙤𝙩"
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
@@ -157,9 +157,9 @@ global.eg = '╰⊱💚⊱ *𝙀́𝙓𝙄𝙏𝙊 | 𝙎𝙐𝘾𝘾𝙀𝙎�
 global.exito = eg
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
-global.wm = "𝙎𝙞𝙨𝙠𝙚𝙙-𝘽𝙤𝙩"
-global.igfg = "𝙎𝙞𝙨𝙠𝙚𝙙-𝘽𝙤𝙩"
-global.nomorown = "584123989549"
+global.wm = "𝙈𝙤𝙣𝙤𝙎𝙚𝙭𝙤-𝘽𝙤𝙩"
+global.igfg = "𝙈𝙤𝙣𝙤𝙎𝙚𝙭𝙤-𝘽𝙤𝙩"
+global.nomorown = "5217202953045"
 global.pdoc = ["application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.ms-excel", "application/msword", "application/pdf", "text/rtf"]
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
