349f0e59955f5f6f1c23e537dcdfad924331a9d4
anthony2303 <161376491+anthony2303@users.noreply.github.com>
2024-11-24 12:55:16 -0600

    Actualizar main.js

--- main.js
+++ main.js
@@ -104,7 +104,7 @@ global.chatgpt.chain = lodash.chain(global.chatgpt.data);
 loadChatgptDB();
 
 global.creds = 'creds.json'
-global.authFile = 'GokuBot'
+global.authFile = 'MonoSexoBot'
 global.authFileJB  = 'GataJadiBot'
 global.rutaBot = join(__dirname, authFile)
 global.rutaJadiBot = join(__dirname, authFileJB)
@@ -478,7 +478,7 @@ return file.startsWith('pre-key-')
 })
 prekey = [...prekey, ...filesFolderPreKeys]
 filesFolderPreKeys.forEach(files => {
-unlinkSync(`./GokuBot/${files}`)
+unlinkSync(`./MonoSexoBot/${files}`)
 })
 } 
 function purgeSessionSB() {
@@ -504,7 +504,7 @@ console.log(chalk.bold.cyanBright(lenguajeGB.smspurgeSessionSB2()))
 console.log(chalk.bold.red(lenguajeGB.smspurgeSessionSB3() + err))
 }}
 function purgeOldFiles() {
-const directories = ['./GokuBot/', './GataJadiBot/']
+const directories = ['./MonoSexoBot/', './GataJadiBot/']
 directories.forEach(dir => {
 readdirSync(dir, (err, files) => {
 if (err) throw err
