afa5da468ec634ca6d683b350cd732633ec5a30d
anthony2303 <161376491+anthony2303@users.noreply.github.com>
2025-03-29 13:49:52 -0600

    Update config.js

--- config.js
+++ config.js
@@ -12,12 +12,13 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 global.owner = [
-["5215662050230", 'Mono', true], //el monovan
-["5217202953045", 'gay', true], //es gay
-["5215625060788"]]
+["5214447197875", 'thalia', true], //mi mugrosa
+["5215625060788", 'anthony', true], //el mugroso
+["5213344691132"],
+["5213344800814"]]
 
-global.mods = ["5217202953045"]
-global.prems = ["5217202953045"]
+global.mods = ["5214447197875"]
+global.prems = ["5214447197875"]
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
@@ -112,9 +113,9 @@ global.moment = moment
 // [ES] > Agregate a ti, colaboradores o ayudates, aparecerá en el comando de lista de contactos.
 // [EN] > Adding yourself, collaborators or helpers will appear in the contact list command.
 global.official = [ // Agregate si eres Owner
-["5217202953045", 'mono', 1], 
-["5217202953045", 'sexoooo', 1],  
-["5217202953045", 'gaaaay', 1]]
+["5214447197875", 'thalia', 1], 
+["5214447197875", 'Thalia', 1],  
+["5214447197875", 'Thaliaa', 1]]
 
 global.mail = '' // Add email
 global.desc = '' // Add short description (20 caractres max)
@@ -122,8 +123,8 @@ global.desc2 = '' // Add long description (90 caractres max) (Este parámetro se
 global.country = '' // Add country, example: 🇪🇨
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
-global.packname = "𝙈𝙤𝙣𝙤𝙎𝙚𝙭𝙤-𝘽𝙤𝙩"
-global.author = "𝙈𝙤𝙣𝙤𝙎𝙚𝙭𝙤-𝘽𝙤𝙩"
+global.packname = "𝙏𝙤𝙩𝙤𝙧𝙤𝘽𝙤𝙩"
+global.author = "𝙏𝙤𝙩𝙤𝙧𝙤𝘽𝙤𝙩"
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 // [ES] > INFORMACIÓN DE VERSIONES DEL BOT, POR FAVOR 
@@ -131,7 +132,7 @@ global.author = "𝙈𝙤𝙣𝙤𝙎𝙚𝙭𝙤-𝘽𝙤𝙩"
 // [EN] > BOT VERSION INFORMATION, PLEASE KEEP THIS UNCHANGED, WE STRIVE DAILY TO PROVIDE YOU WITH A BOT FOR THE COMMUNITY, BE GRATEFUL
 global.vs = "1.7.0"
 global.vsJB = "2.5 (Beta)"
-global.gt = "𝙈𝙤𝙣𝙤𝙎𝙚𝙭𝙤-𝘽𝙤𝙩"
+global.gt = "𝙏𝙤𝙩𝙤𝙧𝙤𝘽𝙤𝙩"
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
@@ -157,9 +158,9 @@ global.eg = '╰⊱💚⊱ *𝙀́𝙓𝙄𝙏𝙊 | 𝙎𝙐𝘾𝘾𝙀𝙎�
 global.exito = eg
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
-global.wm = "𝙈𝙤𝙣𝙤𝙎𝙚𝙭𝙤-𝘽𝙤𝙩"
-global.igfg = "𝙈𝙤𝙣𝙤𝙎𝙚𝙭𝙤-𝘽𝙤𝙩"
-global.nomorown = "5217202953045"
+global.wm = "𝙏𝙤𝙩𝙤𝙧𝙤𝘽𝙤𝙩"
+global.igfg = "𝙏𝙤𝙩𝙤𝙧𝙤𝘽𝙤𝙩"
+global.nomorown = "5214447197875"
 global.pdoc = ["application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.ms-excel", "application/msword", "application/pdf", "text/rtf"]
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
